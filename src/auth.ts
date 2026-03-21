5bca9ee598a1a4255c04819f6edf95e784c06a2b
patrickstanciu <patrickstanciu@yahoo.com>
2026-03-21 15:10:20 +0200

    Fix login redirect

--- src/auth.ts
+++ src/auth.ts
@@ -6,6 +6,7 @@ import { prisma } from "@/lib/prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  trustHost: true,
   providers: [
     Google,
     Resend({
